9a716af0228c0bf5134185a27e3b4ece160e4e29
Yevhen Khomiak <khomyak_eugene@yahoo.com>
2015-12-23 20:29:11 +0200

    save Unit3

---
+++ MyGoITTasks/Unit3/src/com/company/files/AudioFile.java
@@ -0,0 +1,30 @@
+package com.company.files;
+
+import com.company.files.SimpleFile;
+import com.company.utils.Const;
+import sun.audio.AudioData;
+
+/**
+ * Created by Yevgen on 20.12.2015.
+ */
+public class AudioFile extends SimpleFile {
+    AudioData audioData = null;
+
+    public AudioFile(String fileName) {
+        super(fileName, Const.AUDIO_FILE_TYPE);
+    }
+
+    public AudioFile(String fileName, AudioData audioData) {
+        this(fileName);
+
+        setAudioData(audioData);
+    }
+
+    public AudioData getAudioData() {
+        return audioData;
+    }
+
+    public void setAudioData(AudioData audioData) {
+        this.audioData = audioData;
+    }
+}
