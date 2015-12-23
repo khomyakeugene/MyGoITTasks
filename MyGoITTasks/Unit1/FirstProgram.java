51127acdc1b3ecea5ed1ef0b5c679e04cebec732
Yevhen Khomiak <khomyak_eugene@yahoo.com>
2015-12-23 21:09:57 +0200

    Save Unit1

---
+++ MyGoITTasks/Unit1/FirstProgram.java
@@ -0,0 +1,6 @@
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hi, I'm your first Java program! Stay tuned!"); //Display the string.
+    }
+}
